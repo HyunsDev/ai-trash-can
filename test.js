e38b6fc71537fce645d1be43477c139d30b5fbdc
혀느현스 <hyuns@hyuns.dev>
2021-11-19 20:05:16 +0900

    .

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ setInterval(() => {
 
   pulseWidth += increment;
   if (pulseWidth >= 1700) {
-    increment = -100;
+    increment = -10;
   } else if (pulseWidth <= 500) {
-    increment = 100;
+    increment = 10;
   }
 }, 10);
