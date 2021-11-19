82d03d7013146c73bcffb2c562768756c500a9b2
혀느현스 <hyuns@hyuns.dev>
2021-11-19 20:07:28 +0900

    .

--- test.js
+++ test.js
@@ -2,16 +2,8 @@ const Gpio = require('pigpio').Gpio;
 
 const motor = new Gpio(4, {mode: Gpio.OUTPUT});
 
-let pulseWidth = 1000;
-let increment = 100;
-
+let a = true
 setInterval(() => {
-  motor.servoWrite(pulseWidth);
-
-  pulseWidth += increment;
-  if (pulseWidth >= 1700) {
-    increment = -1;
-  } else if (pulseWidth <= 500) {
-    increment = 1;
-  }
-}, 1);
+  motor.servoWrite(a ? 1400 : 500);
+  a = !a
+}, 1500);
