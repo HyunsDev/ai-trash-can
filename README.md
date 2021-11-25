efa8f3bfde4f719970a9de3fe20f58d28ec1ed11
혀느현스 <hyuns@hyuns.dev>
2021-11-25 16:17:50 +0900

    모터 구동 프로그램 개선

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ai-trash-can
  인공지능으로 쓰레기 구분하기
+
+# 개발 노트
+## motor.js 파일의 motorInfo 구조
+```json
+{
+    "category": [
+        {
+            "motor": "모터 객체",
+            "isOpen": true
+        }
+    ]
+}
+```
--- motor.js
+++ motor.js
@@ -6,48 +6,104 @@ function sleep(ms) {
   return new Promise((r) => setTimeout(r, ms));
 }
 
+// 모터 클래스
+class Motor {
+  constructor (gpio) {
+    this.motors = gpio.map(e => {
+      new Gpio(e, {mode: Gpio.OUTPUT})
+    })
+    this.isOpen = false
+  }
+
+  open(force=false) {
+    if (!this.isOpen || force) {
+      this.motors.forEach(e => {
+        e.servoWrite(motorList.config['open-angle']);
+      });
+      return true
+    } else {
+      return false
+    }
+  }
+
+  close(force=false) {
+    if (this.isOpen || force) {
+      this.motors.forEach(e => {
+        e.servoWrite(motorList.config['close-angle']);
+      });
+      return true
+    } else {
+      return false
+    }
+  }
+
+}
+
+
 // 모터 로딩
 const motorInfo = {}
 for (let model in motorList) {
-  motorInfo[model] = []
-  for (let motor of motorList[model].motors) {
-    motorInfo[model].push(new Gpio(motor, {mode: Gpio.OUTPUT}))
-  }
+  const motor = new Motor(gpio)
+  motorInfo[model] = motor
+
+  // 모터 테스트
+  motor.open()
+  sleep(500)
+  motor.close()
+  sleep(500)
 }
 
-// 모터 테스트
+// 실행
 ;(async () => {
-  for (let model in motorInfo) {
-    for (let motor of motorInfo[model]) {
-      console.log(model)
-      motor.servoWrite(2000);
-      await sleep(1000)
-      motor.servoWrite(700);
-      await sleep(1000)
-    }
-    await sleep(1000)
-  }
-})()
+  while (true) {
+    try {
+      const jsonFile = fs.readFileSync('data.txt', 'utf8');
+      const jsonData = JSON.parse(jsonFile);
 
-;(async () => {
-    while (true) {
-        const jsonFile = fs.readFileSync('data.txt', 'utf8');
-        const jsonData = JSON.parse(jsonFile);
-        
-        if (jsonData.status == "found") {
-            console.log(jsonData.kind)
-            for (let kind in motorList) {
-                for (let motor of motorInfo[kind]) {
-                  if (kind == jsonData.kind) {
-                    motor.servoWrite(2400);
-                  } else {
-                    motor.servoWrite(600);
-                  }
-              }
-            }
-        } else {
-          motor.servoWrite(600);
+      if (jsonData.status == "found") {
+        console.log(`${jsonData.kind}을 발견하였습니다.`)
+        for (let kind in motorList) {
+          if (kind == jsonData.kind) { motorInfo[kind].open() }
+          else { motorInfo[kind].close() }
         }
-        await sleep(200)
+      } else {
+        for (let kind in motorList) {
+          motorInfo[kind].close()
+        }
+      }
+    } catch (err) {
+      console.error(err)
     }
-})()
+  }
+})
+
+
+
+// ;(async () => {
+//     while (true) {
+//         const jsonFile = fs.readFileSync('data.txt', 'utf8');
+//         let jsonData
+//         try {
+//           jsonData = JSON.parse(jsonFile);
+//         } catch (err) {
+
+//         }
+        
+        
+//         if (jsonData.status == "found") {
+//             console.log(jsonData.kind)
+//             for (let kind in motorList) {
+//                 for (let motor of motorInfo[kind]) {
+//                   if (kind == jsonData.kind) {
+//                     motor.servoWrite(2400);
+//                   } else {
+//                     motor.servoWrite(600);
+//                   }
+//               }
+//             }
+//         } else {
+//           motor.servoWrite(600);
+//         }
+//         await sleep(200)
+//     }
+// })()
